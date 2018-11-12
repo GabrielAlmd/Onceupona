9752e71e93406c0c225809aa9a76e6322f460ff4
Gabriel Almeida <a34580@ubi.pt>
2018-11-12 22:37:19 +0000

    Initial commit

---
+++ app/src/main/java/pt/ubi/di/st/onceupona/Joga.java
@@ -0,0 +1,13 @@
+package pt.ubi.di.st.onceupona;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Joga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joga);
+    }
+}
