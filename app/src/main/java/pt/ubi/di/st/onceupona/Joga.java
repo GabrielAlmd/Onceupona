d18090c5e1b8ef68ead54ae43ab0834d12d26cef
GabrielAlmd <33692448+GabrielAlmd@users.noreply.github.com>
2018-11-27 17:27:11 +0000

    ola

--- app/src/main/java/pt/ubi/di/st/onceupona/Joga.java
+++ app/src/main/java/pt/ubi/di/st/onceupona/Joga.java
@@ -1,9 +1,10 @@
 package pt.ubi.di.st.onceupona;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Joga extends AppCompatActivity {
+public class Joga extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
