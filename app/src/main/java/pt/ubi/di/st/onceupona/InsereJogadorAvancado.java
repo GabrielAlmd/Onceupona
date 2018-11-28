bf02ae172ae598a3d311696df6e709723859728a
Gabriel Almeida <a34580@ubi.pt>
2018-11-28 17:40:07 +0000

    Alterado:
    Atividade de adicionar jogadores no modo basico.
    Atividade de adicionar jogadores no modo avançado.

---
+++ app/src/main/java/pt/ubi/di/st/onceupona/InsereJogadorAvancado.java
@@ -0,0 +1,18 @@
+package pt.ubi.di.st.onceupona;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class InsereJogadorAvancado extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insere_jogador_avancado);
+    }
+
+    public void vamosJogarModo (View view){ //onclik metodo do botão seguinte
+
+    }
+}
