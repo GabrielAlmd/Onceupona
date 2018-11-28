deb64219abb3ba1aa9ed7b503a610dc9fb848cec
Gabriel Almeida <a34580@ubi.pt>
2018-11-28 17:14:52 +0000

    Atividade de adicionar jogadores no modo basico.

--- app/src/main/java/pt/ubi/di/st/onceupona/InsereJogadorBasico.java
+++ app/src/main/java/pt/ubi/di/st/onceupona/InsereJogadorBasico.java
@@ -2,6 +2,7 @@ package pt.ubi.di.st.onceupona;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InsereJogadorBasico extends Activity {
 
@@ -9,5 +10,9 @@ public class InsereJogadorBasico extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insere_jogador_basico);
+    }
+
+    public void vamosJogar (View view){ //onclik metodo do botão seguinte
+
     }
 }
