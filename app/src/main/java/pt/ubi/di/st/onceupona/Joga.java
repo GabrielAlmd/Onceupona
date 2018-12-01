65cc7132c660a98cb12070202e68e2555247c0ff
JoaoVieira96 <joaomgv96@gmail.com>
2018-12-01 22:02:08 +0000

    po

--- app/src/main/java/pt/ubi/di/st/onceupona/Joga.java
+++ app/src/main/java/pt/ubi/di/st/onceupona/Joga.java
@@ -3,14 +3,19 @@ package pt.ubi.di.st.onceupona;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Joga extends Activity { //boas duas vezes
+
+    TextView tPlayers = findViewById(R.id.lable2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joga);
     }
+
+
+
 }
 
-//ola
