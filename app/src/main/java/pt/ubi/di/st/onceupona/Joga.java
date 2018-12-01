9b4965499ec2ae28fd76043469294a4181415b7d
JoaoVieira96 <joaomgv96@gmail.com>
2018-12-01 22:29:44 +0000

    Teste

--- app/src/main/java/pt/ubi/di/st/onceupona/Joga.java
+++ app/src/main/java/pt/ubi/di/st/onceupona/Joga.java
@@ -1,6 +1,7 @@
 package pt.ubi.di.st.onceupona;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -8,11 +9,17 @@ import android.widget.TextView;
 public class Joga extends Activity { //boas duas vezes
 
     TextView tPlayers = findViewById(R.id.lable2);
+    Intent iPreviousAct;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joga);
+        iPreviousAct=getIntent();
+
+
+
     }
 
 
